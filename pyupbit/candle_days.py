3dc20989f9360f37f49aad0e93e9ce8d58574847
john <snj830526@gmail.com>
2021-04-17 22:22:13 +0900

    코드 좀 정리

--- pyupbit/candle_days.py
+++ pyupbit/candle_days.py
@@ -19,21 +19,23 @@ def view_candle_day(market="KRW-BTC", market_name=""):
 # 잘 될 것 같은 코인 계산
 def get_rocketboosting_coins(candle_data, market_name):
     d = candle_data
+    # 코인 코드
+    market = pyupbit.get_market(d)
     # 목표 코인 단가( 오늘 시작가 + (어제 고가 - 어제 저가) * 0.5 )
-    target_price = get_target_price_to_buy(market_name)
+    target_price = get_target_price_to_buy(market)
     # 코인 현재 단가
-    current_price = pyupbit.view_candle_min(d[0]['market'])[0]['trade_price']
+    current_price = pyupbit.get_current_coin_price(d)
     # 현재 코인 단가가 목표가 보다 높고 단가가 1원 이상인 코인만 필터
-    if current_price > target_price and d[0]['opening_price'] > 1:
-        coin_info = '현재가 : ' + str(current_price) + '-' + d[0]['market'] + "(" + market_name + "-" + \
-                    str(round(d[0]['change_rate'] * 100, 2)) + '%' + ")" + ' / opening_p : ' + \
-                    str(d[0]['opening_price']) + ' / high_p(오늘[어제]) : ' + \
-                    str(d[0]['high_price']) + '[' + str(d[1]['high_price']) + '] / low_p(오늘[어제]) : ' + \
-                    str(d[0]['low_price']) + '[' + str(d[1]['low_price']) + '] / prev_p : ' + \
-                    str(d[0]['prev_closing_price']) + ' / change_p : ' + \
-                    str(d[0]['change_price'])
+    if current_price > target_price and pyupbit.get_today_opening_price(d) > 1:
+        coin_info = '현재가 : ' + str(current_price) + '-' + market + "(" + market_name + "-" + \
+                    str(pyupbit.get_change_rate(d)) + '%' + ")" + ' / opening_p : ' + \
+                    str(pyupbit.get_today_opening_price(d)) + ' / high_p(오늘[어제]) : ' + \
+                    str(pyupbit.get_today_high_price(d)) + '[' + str(pyupbit.get_yesterday_high_price(d)) + '] / low_p(오늘[어제]) : ' + \
+                    str(pyupbit.get_today_low_price(d)) + '[' + str(pyupbit.get_yesterday_low_price(d)) + '] / prev_p : ' + \
+                    str(pyupbit.get_yesterday_close_price(d)) + ' / change_p : ' + \
+                    str(pyupbit.get_change_price(d))
         print(coin_info)
-        return {round(d[0]['change_rate'] * 100, 2): d[0]['market']}
+        return {pyupbit.get_change_rate(d): market}
     else:
         return None
 
--- pyupbit/common_utils.py
+++ pyupbit/common_utils.py
@@ -1,3 +1,6 @@
+import pyupbit
+
+
 # 분 단위 코인정보로 현재 코인 단가 추출(매입, 매도 시 코인 단가 추출용)
 def get_current_coin_price(coin_info=[]):
     return float(coin_info[0]['trade_price'])
@@ -38,3 +41,52 @@ def get_profit_rate(current_unit_price=0, buy_unit_price=0):
         return 0
     else:
         return round((current_unit_price / buy_unit_price * 100), 2)
+
+
+# 일 캔들에서 값 추출(코인 코드)
+def get_market(candle):
+    return candle[0]['market']
+
+
+# 일 캔들에서 값 추출(현재 코인 단가)
+def get_current_coin_price(candle):
+    return pyupbit.view_candle_min(candle[0]['market'])[0]['trade_price']
+
+
+# 일 캔들에서 값 추출(오늘 시작 코인 단가)
+def get_today_opening_price(candle):
+    return candle[0]['opening_price']
+
+
+# 일 캔들에서 값 추출(변동률)
+def get_change_rate(candle):
+    return round(candle[0]['change_rate'] * 100, 2)
+
+
+# 일 캔들에서 값 추출(오늘 고가)
+def get_today_high_price(candle):
+    return candle[0]['high_price']
+
+
+# 일 캔들에서 값 추출(오늘 고가)
+def get_yesterday_high_price(candle):
+    return candle[1]['high_price']
+
+
+# 일 캔들에서 값 추출(오늘 저가)
+def get_today_low_price(candle):
+    return candle[0]['low_price']
+
+
+# 일 캔들에서 값 추출(어제 저가)
+def get_yesterday_low_price(candle):
+    return candle[1]['low_price']
+
+
+# 일 캔들에서 값 추출(어제 마감가)
+def get_yesterday_close_price(candle):
+    return candle[0]['prev_closing_price']
+
+
+def get_change_price(candle):
+    return candle[0]['change_price']
